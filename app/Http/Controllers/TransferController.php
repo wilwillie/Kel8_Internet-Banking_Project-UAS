490c60ee1d17bcaf48334d5efca3562c0af4bdb6
Ryookie <mario.535230156@stu.untar.ac.id>
2024-06-28 13:03:56 +0700

    Mario Alvino

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TransferController extends Controller
-{
+{   
     public function index()
-    {
+    {   
         // Logic to fetch data or perform actions related to transfer
-        
+
         // Return view with data if needed
         return view('transfer.index');
-    }
-}           
+    }   
+}     
