f9a55563b6c78716e16c971c32802cf2d035908a
AkbarIqbnuPrayoga <prayogaiqbnu@gmail.com>
2024-06-28 14:16:00 +0700

    Akbar Iqbnu

--- app/Models/Withdraw.php
+++ app/Models/Withdraw.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Model;
 
-class Withdraw extends Model
-{
-         /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'withdraws';
-}
+    class Withdraw extends Model
+    {
+            /**
+         * The table associated with the model.
+         *
+         * @var string
+         */
+        protected $table = 'withdraws';
+    }
