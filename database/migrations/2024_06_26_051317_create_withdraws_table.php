c892773fb01f14f133fa68a402d55f7e588017dd
AkbarIqbnuPrayoga <prayogaiqbnu@gmail.com>
2024-06-28 14:12:15 +0700

    Akbar iqbnu

--- database/migrations/2024_06_26_051317_create_withdraws_table.php
+++ database/migrations/2024_06_26_051317_create_withdraws_table.php
@@ -1,32 +1,32 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        if (! Schema::hasTable('withdraws')) {
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            if (! Schema::hasTable('withdraws')) {
 
-            Schema::create('withdraws', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->integer('user_id')->unsigned();
-                $table->float('amount', 8,2);   
-                $table->timestamps();
-            });
+                Schema::create('withdraws', function (Blueprint $table) {
+                    $table->bigIncrements('id');
+                    $table->integer('user_id')->unsigned();
+                    $table->float('amount', 8,2);   
+                    $table->timestamps();
+                });
+            }
         }
-    }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('withdraws');
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('withdraws');
+        }
+    };
